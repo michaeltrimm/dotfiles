ea55b51c32bcdb934f5fc57b56f31b8d9d314196
Michael A. Trimm <michaeltrimm@users.noreply.github.com>
2015-10-18 14:08:21 -0400

    Update set_privacy_preferences.sh

--- os/ubuntu/preferences/set_privacy_preferences.sh
+++ os/ubuntu/preferences/set_privacy_preferences.sh
@@ -5,22 +5,9 @@ cd "$(dirname "$BASH_SOURCE")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-set_preferences() {
-
-    execute 'gsettings set com.canonical.Unity.Lenses remote-content-search "none"' \
-        'Turn off "Remote Search" so that search terms in Dash do not get sent over the internet'
-
-    execute 'gsettings set com.canonical.Unity.ApplicationsLens display-available-apps false' \
-        'Disable Dash "More suggestions" section'
-
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 main() {
 
     print_in_purple '  Privacy\n\n'
-    set_preferences
 
 }
 

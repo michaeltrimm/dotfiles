eafab6a8d954ec9336993855655962818c394e86
Michael A. Trimm <michaeltrimm@users.noreply.github.com>
2015-10-18 14:09:07 -0400

    Update install_applications.sh

--- os/install_applications.sh
+++ os/install_applications.sh
@@ -7,18 +7,6 @@ cd "$(dirname "$BASH_SOURCE")" \
 
 main() {
 
-    declare -r OS="$(get_os)"
-
-    if [ "$OS" == 'osx' ]; then
-
-        ./os_x/installs/main.sh
-        ./os_x/change_default_bash_version.sh
-
-    elif [ "$OS" == 'ubuntu' ]; then
-
-        ./ubuntu/installs/main.sh
-
-    fi
 
 }
 
